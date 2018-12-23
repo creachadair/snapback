7ad0c6669462c1a01b1d408aa791bff79ef532c8
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-12-23 10:55:14 -0800

    Switch config to use YAML instead of raw JSON.
    The parser should still accept the JSON version, but it's a little easier to
    edit as YAML. Like C++, it's fine if you avoid the corners.

--- config/config.go
+++ config/config.go
@@ -2,37 +2,39 @@
 package config
 
 import (
-	"encoding/json"
 	"io"
 
 	"bitbucket.org/creachadair/tarsnap"
+	yaml "gopkg.in/yaml.v2"
 )
 
 // A Config contains settings for the snapback tool.
 type Config struct {
 	// An ordered list of backups to be created.
-	Backup []Backup `json:"backup"`
+	Backup []Backup `yaml:"backup"`
 
 	// Working directory from which backups should be run.  This can be
 	// overridden by individual backups. If this is not specified, the directory
 	// containing the configuration file is used.
-	WorkDir string `json:"workDir"`
+	WorkDir string `yaml:"workdir"`
 }
+
 // TODO: Add pruning policy.
 
 // A Backup describes a collection of files to be backed up as a unit together.
 type Backup struct {
 	// The name defines the base name of the archive. A timestamp will be
 	// appended to this name to obtain the complete name.
-	Name string `json:"name"`
+Name string `yaml:"name"`
 
 	// The archive creation options for this backup.
-	tarsnap.CreateOptions
+	tarsnap.CreateOptions `yaml:",inline"`
 }
 
 // Parse decodes a *Config from the specified reader.
 func Parse(r io.Reader) (*Config, error) {
-	dec := json.NewDecoder(r)
+	dec := yaml.NewDecoder(r)
+	dec.SetStrict(true)
 	var cfg Config
 	if err := dec.Decode(&cfg); err != nil {
 		return nil, err
