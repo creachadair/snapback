2614618ae81aed65d5449f856b69ee05c02d9668
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-12-23 10:26:57 -0800

    Move backup settings to a configuration file.
    By default, use ~/.snapback, or override with a --config flag.

--- snapback.go
+++ snapback.go
@@ -10,10 +10,9 @@ import (
 	"time"
 
 	"bitbucket.org/creachadair/shell"
+	"bitbucket.org/creachadair/snapback/config"
 	"bitbucket.org/creachadair/tarsnap"
 )
-
-// TODO: Pull out the config.
 
 // TODO: Prune old backups by age.
 // - Keep everything from the last day.
@@ -21,31 +20,6 @@ import (
 // - Keep 1/week for up to 3 months.
 // - Keep 1/month for up to 12 months.
 // - Keep 1/year after that.
-
-var config = map[string]tarsnap.CreateOptions{
-	"documents": {Include: []string{"Documents", "Desktop", "Downloads"}},
-	"blobdata":  {Include: []string{"data"}},
-	"pictures":  {Include: []string{"Pictures"}},
-	"software": {
-		Include: []string{"software"},
-		Exclude: []string{"software/kythe/**", "software/sourced/**"},
-	},
-	"dotfiles": {
-		Include: []string{".dotfiles"},
-		Modify:  []string{`/^\.//`},
-	},
-	"library": {
-		Include: []string{
-			"Library/Application Support",
-			"Library/Accounts",
-			"Library/Calendars",
-			"Library/Keychains",
-			"Library/Mail",
-			"Library/Preferences",
-		},
-		Exclude: []string{"Library/Application Support/Steam/**"},
-	},
-}
 
 func init() {
 	flag.Usage = func() {
@@ -62,67 +36,78 @@ Options:
 }
 
 var (
-	workDir   = flag.String("dir", os.Getenv("HOME"), "Run from this directory")
-	doList    = flag.Bool("list", false, "List known archives")
-	doSize    = flag.Bool("size", false, "Print size statistics")
-	doDryRun  = flag.Bool("dry-run", false, "Simulate creating archives")
-	doVerbose = flag.Bool("v", false, "Verbose logging")
+	configFile = flag.String("config", "$HOME/.snapback", "Configuration file")
+	doList     = flag.Bool("list", false, "List known archives")
+	doSize     = flag.Bool("size", false, "Print size statistics")
+	doDryRun   = flag.Bool("dry-run", false, "Simulate creating archives")
+	doVerbose  = flag.Bool("v", false, "Verbose logging")
 )
 
 func main() {
 	flag.Parse()
 
-	cfg := &tarsnap.Config{Dir: *workDir}
-	if *doVerbose {
-		cfg.CmdLog = func(cmd string, args []string) {
-			fmt.Fprintf(os.Stderr, "+ [%s] %s\n", cmd, shell.Join(args))
-		}
+	dir, cfg, err := loadConfig(*configFile)
+	if err != nil {
+		log.Fatalf("Loading configuration: %v", err)
+	}
+	ts := &tarsnap.Config{
+		Dir:    os.ExpandEnv(cfg.WorkDir),
+		CmdLog: logCommand,
+	}
+	if ts.Dir == "" {
+		ts.Dir = dir
 	}
 
 	if *doList {
-		as, err := cfg.Archives()
-		if err != nil {
-			log.Fatalf("Listing archives: %v", err)
-		}
-		for _, arch := range as {
-			if matchExpr(arch.Name, flag.Args()) {
-				fmt.Printf("%s\t%s\n", arch.Created.Format(time.RFC3339), arch.Name)
-			}
-		}
+		listArchives(ts)
 		return
 	} else if *doSize {
-		info, err := cfg.Size(flag.Args()...)
-		if err != nil {
-			log.Fatalf("Reading stats: %v", err)
-		}
-		fmt.Printf("TOTAL\t%d\t%d\t%d\t%d\n", info.All.InputBytes, info.All.CompressedBytes,
-			info.All.UniqueBytes, info.All.CompressedUniqueBytes)
-		for arch, size := range info.Archive {
-			fmt.Printf("%s\t%d\t%d\t%d\t%d\n", arch, size.InputBytes, size.CompressedBytes,
-				size.UniqueBytes, size.CompressedUniqueBytes)
-		}
+		printSizes(ts)
 		return
 	} else if flag.NArg() != 0 {
 		log.Fatalf("Extra arguments after command: %v", flag.Args())
 	}
 
 	start := time.Now()
-	if err := createBackups(cfg, config); err != nil {
+	if err := createBackups(ts, cfg); err != nil {
 		log.Fatalf("Failed: %v", err)
 	}
 	log.Printf("Backups finished [%v elapsed]", time.Since(start).Round(time.Second))
 }
 
-func createBackups(cfg *tarsnap.Config, config map[string]tarsnap.CreateOptions) error {
+func listArchives(ts *tarsnap.Config) {
+	as, err := ts.Archives()
+	if err != nil {
+		log.Fatalf("Listing archives: %v", err)
+	}
+	for _, arch := range as {
+		if matchExpr(arch.Name, flag.Args()) {
+			fmt.Printf("%s\t%s\n", arch.Created.Format(time.RFC3339), arch.Name)
+		}
+	}
+}
+
+func printSizes(ts *tarsnap.Config) {
+	info, err := ts.Size(flag.Args()...)
+	if err != nil {
+		log.Fatalf("Reading stats: %v", err)
+	}
+	fmt.Printf("TOTAL\t%d\t%d\t%d\t%d\n", info.All.InputBytes, info.All.CompressedBytes,
+		info.All.UniqueBytes, info.All.CompressedUniqueBytes)
+	for arch, size := range info.Archive {
+		fmt.Printf("%s\t%d\t%d\t%d\t%d\n", arch, size.InputBytes, size.CompressedBytes,
+			size.UniqueBytes, size.CompressedUniqueBytes)
+	}
+}
+
+func createBackups(ts *tarsnap.Config, cfg *config.Config) error {
 	tag := "." + time.Now().Format("20060102-1504")
 	nerrs := 0
-	for base, opts := range config {
-		if *doDryRun {
-			opts.DryRun = true
-		}
-		name := base + tag
-		err := cfg.Create(name, opts)
-		if err != nil {
+	for _, b := range cfg.Backup {
+		opts := b.CreateOptions
+		opts.DryRun = *doDryRun
+		name := b.Name + tag
+		if err := ts.Create(name, opts); err != nil {
 			log.Printf("ERROR: %s: %v", name, err)
 			nerrs++
 		}
@@ -141,4 +126,24 @@ func matchExpr(name string, exprs []string) bool {
 		}
 	}
 	return len(exprs) == 0
+}
+
+func loadConfig(path string) (string, *config.Config, error) {
+	loc, err := filepath.Abs(os.ExpandEnv(path))
+	if err != nil {
+		return "", nil, err
+	}
+	f, err := os.Open(loc)
+	if err != nil {
+		return "", nil, err
+	}
+	defer f.Close()
+	cfg, err := config.Parse(f)
+	return filepath.Dir(loc), cfg, err
+}
+
+func logCommand(cmd string, args []string) {
+	if *doVerbose {
+		fmt.Fprintf(os.Stderr, "+ [%s] %s\n", cmd, shell.Join(args))
+	}
 }
