b20a93706b5fe625ae4a0f652e1e18a8fc6fbab6
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-03-28 11:01:59 -0700

    Add LICENSE and copyright notices.

--- config/config.go
+++ config/config.go
@@ -1,3 +1,5 @@
+// Copyright (C) 2018 Michael J. Fromberger. All Rights Reserved.
+
 // Package config describes configuration settings for the snapback tool.
 package config
 
--- snapback.go
+++ snapback.go
@@ -1,3 +1,5 @@
+// Copyright (C) 2018 Michael J. Fromberger. All Rights Reserved.
+
 // Program snapback creates tarsnap backups of important directories.
 package main
 
